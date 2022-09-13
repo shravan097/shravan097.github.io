df9d22ee1b0150eace66fe528d216dc726312920
shravan097 <shravan097@users.noreply.github.com>
2022-09-12 23:04:32 -0700

    Fix bug; Add Education

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,30 +1,35 @@
+import { useLocation } from "@reach/router"
+import { Link } from "gatsby"
 import * as React from "react"
 import { Cross } from "../Icons/cross"
 import { Hamburger } from "../Icons/hamburger"
 
 export const Navbar = () => {
   const [open, setOpen] = React.useState(false)
+  const location = useLocation()
+  JSON.stringify(location)
   return (
     <>
-    <nav className="hidden sm:flex w-auto flex-col justify-center h-full bg-zinc-800 shadow-2xl absolute overflow-hidden">
+    <base href="https://www.shravan097.github.io" />
+    <nav className="hidden sm:flex w-auto flex-col justify-center h-full bg-zinc-800 shadow-2xl fixed overflow-hidden">
         <div className="flex flex-row sm:flex-col items-center ">
-          <a href='./' className="my-2 mx-16 font-bold text-2xl text-slate-200 hover:underline decoration-gray-500 decoration-4"> Intro </a>
-          <a href='./inprogress' className="my-2 mx-16 font-bold text-2xl text-slate-200 hover:underline decoration-gray-500 decoration-4"> Education </a>
-          <a href='./inprogress' className="my-2 mx-16 font-bold text-2xl text-slate-200 hover:underline decoration-gray-500 decoration-4"> Experience </a>
-          <a className="my-2 mx-16 font-bold text-2xl text-slate-200 hover:underline decoration-gray-500 decoration-4"> Blog </a>
+          <Link to={`${location.origin}/`} className="my-2 mx-16 font-bold text-2xl text-slate-200 hover:underline decoration-gray-500 decoration-4"> Intro </Link>
+          <Link to={`${location.origin}/education/`} className="my-2 mx-16 font-bold text-2xl text-slate-200 hover:underline decoration-gray-500 decoration-4"> Education </Link>
+          <Link to={`${location.origin}/inprogress/`} className="my-2 mx-16 font-bold text-2xl text-slate-200 hover:underline decoration-gray-500 decoration-4"> Experience </Link>
+          <Link to={`${location.origin}/inprogress/`} className="my-2 mx-16 font-bold text-2xl text-slate-200 hover:underline decoration-gray-500 decoration-4"> Blog </Link>
         </div>
     </nav>
 
     {/* Mobile */}
-    <nav className="flex sm:hidden flex-col w-screen h-auto bg-zinc-800 shadow-2xl absolute overflow-hidden items-start">
+    <nav className="flex sm:hidden flex-col w-screen h-auto bg-zinc-800 shadow-2xl fixed overflow-hidden items-start">
       <button type="button" className="mx-4 my-4 rounded-md text-gray-400 hover:text-white " aria-controls="mobile-menu" aria-expanded="true" onClick={() => setOpen(!open)}>
         { open ? <Cross/> : <Hamburger/> }
       </button>
       {/* todo - simply this open &&  */}
-      {open && <a href='./' className="mx-4 my-2 font-bold text-2xl text-slate-200 hover:underline decoration-gray-500 decoration-4"> Intro </a>}
-      {open && <a href='./inprogress' className="mx-4 my-2 font-bold text-2xl text-slate-200 hover:underline decoration-gray-500 decoration-4"> Education </a>}
-      {open && <a href='./inprogress' className="mx-4 my-2 font-bold text-2xl text-slate-200 hover:underline decoration-gray-500 decoration-4"> Experience </a>}
-      {open && <a href='./inprogress' className="mx-4  my-2 font-bold text-2xl text-slate-200 hover:underline decoration-gray-500 decoration-4"> Blog </a>}
+      {open && <Link to={`${location.origin}/`} className="mx-4 my-2 font-bold text-2xl text-slate-200 hover:underline decoration-gray-500 decoration-4"> Intro </Link>}
+      {open && <Link to={`${location.origin}/education/`} className="mx-4 my-2 font-bold text-2xl text-slate-200 hover:underline decoration-gray-500 decoration-4" replace> Education </Link>}
+      {open && <Link to={`${location.origin}/inprogress/`} className="mx-4 my-2 font-bold text-2xl text-slate-200 hover:underline decoration-gray-500 decoration-4"> Experience </Link>}
+      {open && <Link to={`${location.origin}/inprogress/`} className="mx-4  my-2 font-bold text-2xl text-slate-200 hover:underline decoration-gray-500 decoration-4"> Blog </Link>}
     </nav>
     </>
     
--- src/components/Pages/education.tsx
+++ src/components/Pages/education.tsx
@@ -3,8 +3,19 @@ import { Linkedin } from "../Icons/linkedin"
 
 export const Education = () =>  {
   return (
-    <div className="container flex justify-center">
-      <p className="text-5xl">Education</p>
+    <>
+    <div className="my-4 flex justify-center flex-row">
+      <div className="mx-2">
+        <img className='h-16 w-full rounded-sm' alt='ccny logo' src='https://upload.wikimedia.org/wikipedia/commons/2/25/CCNY_logo_flush_left.svg' />
+      </div>
+      <div className="h-full mx-2 border-r-2 justify-center border-zinc-400" />
+      <div className="mx-2 justify center flex-col">
+        <p className="font-mono text-xl">BS Computer Science</p>
+        <p className="font-mono text-xl text-center">2019</p>
+      </div>
+      
     </div>
+    </>
+    
   )
 }
