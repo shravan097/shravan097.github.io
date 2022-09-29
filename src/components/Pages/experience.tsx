084b6cb603144b73527a119feb01215aeb7d4dca
Shravan Dhakal <sadijdhakal@gmail.com>
2022-09-29 00:06:58 -0700

    v1.2 (#7)
    Co-authored-by: shravan097 <shravan097@users.noreply.github.com>

--- src/components/Pages/experience.tsx
+++ src/components/Pages/experience.tsx
@@ -1,62 +1,61 @@
 import * as React from "react"
 import { SocialLogos } from "./socialLogos"
 
-export const Experience = () =>  {
+export const Experience = React.forwardRef((_props, ref: React.LegacyRef<HTMLDivElement>) =>  {
   // TODO - REPLACE LOGO HERE
   return (
-    <>
-    <div className="m-4 flex justify-evenly flex-col space-evenly">
-      <div>
-        <h1 className="font-mono text-2xl">Backend Development</h1>
-        <ul className="list-disc">
-          <li className="mx-8 font-mono text-xl">
-            Microservice, Monolithic, Serverless Architecture
-          </li>
-          <li className="mx-8 font-mono text-xl">
-            Message Queues, RESTful, GraphQL
-          </li>
-          <li className="mx-8 font-mono text-xl">
-            AWS
-          </li>
-        </ul>
-        <p ></p>
+    <div ref={ref} className="snap-start flex h-full w-full justify-center items-center">
+      <div className="mx-4 flex flex-col">
+        <div>
+          <h1 className="font-mono text-2xl">Backend Development</h1>
+          <ul className="list-disc">
+            <li className="mx-8 font-mono text-xl">
+              Microservice, Monolithic, Serverless Architecture
+            </li>
+            <li className="mx-8 font-mono text-xl">
+              Message Queues, RESTful, GraphQL
+            </li>
+            <li className="mx-8 font-mono text-xl">
+              AWS
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h1 className="mt-8 font-mono text-2xl">Fields</h1>
+          <ul className="list-disc">
+            <li className="mx-8 font-mono text-xl">
+              Automotive IoT
+            </li>
+            <li className="mx-8 font-mono text-xl">
+              Healthtech 
+            </li>
+            <li className="mx-8 font-mono text-xl">
+              Fintech
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h1 className="mt-8 font-mono text-2xl">Tech</h1>
+          <ul className="list-disc">
+            <li className="mx-8 font-mono text-xl">
+              Typescript
+            </li>
+            <li className="mx-8 font-mono text-xl">
+              Python 
+            </li>
+            <li className="mx-8 font-mono text-xl">
+              Ruby
+            </li>
+            <li className="mx-8 font-mono text-xl">
+              React
+            </li>
+            <li className="mx-8 font-mono text-xl">
+              Redux
+            </li>
+          </ul>
+        </div>
+        <SocialLogos/>
       </div>
-      <div>
-        <h1 className="mt-8 font-mono text-2xl">Fields</h1>
-        <ul className="list-disc">
-          <li className="mx-8 font-mono text-xl">
-            Automotive IoT
-          </li>
-          <li className="mx-8 font-mono text-xl">
-            Healthtech 
-          </li>
-          <li className="mx-8 font-mono text-xl">
-            Fintech
-          </li>
-        </ul>
-      </div>
-      <div>
-        <h1 className="mt-8 font-mono text-2xl">Tech</h1>
-        <ul className="list-disc">
-          <li className="mx-8 font-mono text-xl">
-            Typescript
-          </li>
-          <li className="mx-8 font-mono text-xl">
-            Python 
-          </li>
-          <li className="mx-8 font-mono text-xl">
-            Ruby
-          </li>
-          <li className="mx-8 font-mono text-xl">
-            React
-          </li>
-          <li className="mx-8 font-mono text-xl">
-            Redux
-          </li>
-        </ul>
-      </div>
-      <SocialLogos/>
     </div>
-    </>
   )
-}
+})
